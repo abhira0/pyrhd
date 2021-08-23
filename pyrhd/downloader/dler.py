24ab664fb773cbb67b830ddf2a15d1acc330f7ac
abhira0 <abhishekr0@protonmail.com>
2021-08-23 22:11:31 +0530

    v0.0.12

--- pyrhd/downloader/dler.py
+++ pyrhd/downloader/dler.py
@@ -1,12 +1,20 @@
 import os
 import shutil
-from threading import BoundedSemaphore
+from threading import BoundedSemaphore, Thread
 
 import requests
+from pyrhd.harvester.hrv8r import BaseHarvester
 from utility.cprint.cprint import aprint
 
 
-class BaseDownloader:
+class BaseDownloader(BaseHarvester):
+    def __init__(self, ultimatum_path) -> None:
+        self.ultimatum_path = ultimatum_path
+        self.ultimatum = {}
+        self.getFileData()
+        self.life_saver_thr = Thread(target=self.lifeSaver, daemon=True)
+        self.life_saver_thr.start()
+
     @classmethod
     def downloadAMedia(
         url: str, dir_: str, title: str, sema4: BoundedSemaphore
@@ -14,16 +22,20 @@ class BaseDownloader:
         url_filename, ex10sion = url.split("/")[-1].split(".")
         ex10sion = ex10sion.split("?")[0] if "?" in ex10sion else ex10sion
         path_ = f"{dir_}\\{title or url_filename}.{ex10sion}"
-        r = requests.get(url, stream=True)
         downloaded = os.path.exists(path_)
-        if r.status_code == 200 and not downloaded:
+        if downloaded:
+            aprint(f"⚠️ Existing media ", "green", url, "magenta")
+            return True
+        r = requests.get(url, stream=True)
+        if r.status_code == 200:
             r.raw.decode_content = True
             with open(path_ + ".part", "wb") as f:
                 shutil.copyfileobj(r.raw, f)
             downloaded = True
             os.rename(path_ + ".part", path_)
-            aprint(f"[+] Downloaded media from ", "green", url, "magenta")
+            aprint(f"✅ Downloaded media from ", "green", url, "magenta")
         if not downloaded:
-            aprint(f"❗ {r.status_code} Error while downloading ", "red", url, "magenta")
+            msg = f"❎ {r.status_code} Error while downloading "
+            aprint(msg, "red", url, "magenta")
         sema4.release()
         return downloaded

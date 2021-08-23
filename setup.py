004ef6bd44f9c9c91b489c63bf6884f8f1563d71
abhira0 <abhishekr0@protonmail.com>
2021-08-23 17:54:07 +0530

    v0.0.3

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ setuptools.setup(
     url="https://github.com/abhira0/pyrhd",
     project_urls={"URL Shortener": "geekcrew.in/s"},
     license="NA",
-    # packages=setuptools.find_packages(),
-    install_requires=["requests", "bs4"],
+    package_dir={"": "pyrhd"},
+    packages=setuptools.find_packages(where="pyrhd"),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: NA :: NA",

e52bf343210540f354fa84de033d44e502194037
abhira0 <abhishekr0@protonmail.com>
2021-08-24 19:53:32 +0530

    v0.0.21 - bug fixes

--- pyrhd/downloader/dler.py
+++ pyrhd/downloader/dler.py
@@ -10,6 +10,7 @@ from pyrhd.utility.cprint import aprint
 
 class BaseDownloader(BaseHarvester):
     def __init__(self, ultimatum_path) -> None:
+        super().__init__(ultimatum_path)
         self.ultimatum_path = ultimatum_path
         self.ultimatum = {}
         self.getFileData()
@@ -18,17 +19,23 @@ class BaseDownloader(BaseHarvester):
 
     @classmethod
     def downloadAMedia(
-        cls, url: str, dir_: str, title: str = None, sema4: BoundedSemaphore = None
+        cls,
+        url: str,
+        dir_: str,
+        title: str = None,
+        sema4: BoundedSemaphore = None,
+        verbose: bool = True,
+        err_ver: bool = True,
     ) -> bool:
         filename, f10sion = os.path.splitext(title or "")
-        url_filename, ex10sion = url.split("/")[-1].split(".")
+        url_filename, ex10sion = os.path.splitext(url.split("/")[-1])
         ex10sion = ex10sion.split("?")[0] if "?" in ex10sion else ex10sion
         path_ = f"{dir_}\\{filename or url_filename}.{ex10sion or f10sion or ''}"
 
         downloaded = os.path.exists(path_)
         if downloaded:
-            aprint(f"⚠️ Existing media ", "green", url, "magenta")
-            sema4.release()
+            aprint(f"⚠️ Existing media ", "green", url, "magenta") if verbose else None
+            sema4.release() if sema4 else None
             return True
 
         # TODO: Add resume functionality for partially downloaded files
@@ -36,27 +43,23 @@ class BaseDownloader(BaseHarvester):
         # files and tries to download from the beginning
         if os.path.exists(path_ + ".part"):
             os.remove(path_ + ".part")
-
+        r = None
         try:
             r = requests.get(url, stream=True)
-        except Exception as e:
-            aprint(e, "red")
-
-        if r and r.status_code == 200:
-            r.raw.decode_content = True
-            with open(path_ + ".part", "wb") as f:
-                shutil.copyfileobj(r.raw, f)
-            aprint(f"✅ Downloaded media from ", "green", url, "magenta")
-            time.sleep(0.5)
-            try:
+            if r and r.status_code == 200:
+                r.raw.decode_content = True
+                with open(path_ + ".part", "wb") as f:
+                    shutil.copyfileobj(r.raw, f)
+                if verbose:
+                    aprint(f"✅ Downloaded media from ", "green", url, "magenta")
+                time.sleep(0.5)
                 os.rename(path_ + ".part", path_)
-            except Exception as e:
-                aprint(e, "red")
-            downloaded = True
+                downloaded = True
+        except Exception as e:
+            aprint(e, "red") if err_ver else None
 
         if not downloaded:
             msg = f"❎ {r.status_code if r else ''} Error while downloading "
-            aprint(msg, "red", url, "magenta")
-        if sema4:
-            sema4.release()
+            aprint(msg, "red", url, "magenta") if verbose else None
+        sema4.release() if sema4 else None
         return downloaded

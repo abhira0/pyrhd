d0c021ff64890f49540457b5a2358c2584c71646
abhira0 <abhishekr0@protonmail.com>
2021-11-20 10:01:10 +0530

    v0.1.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyrhd",
-    version="0.1.8",
+    version="0.1.9",
     author="abhira0",
     author_email="abhira0@protonmail.com",
     description="Scraping Library for Personal Use",

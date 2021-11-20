c10b2f7f52140e1a8e1826c1669325912db78a58
abhira0 <abhishekr0@protonmail.com>
2021-11-20 09:51:26 +0530

    Update cprint.py
    Added:
    1. deleteLines
    
    Modified:
    1. Now aprint can receive optional same_line and flush argument.
    same_line is a flag which allows the next printable statement to be on the same_line as the currently printed statement if set True, else it just prints as in a normal print statement would do.
    flush is sent over to print statements
    2. printInfo now takes all the parameters which are also taken by aprint

--- pyrhd/utility/cprint.py
+++ pyrhd/utility/cprint.py
@@ -1,5 +1,7 @@
+import sys
+from typing import List, Optional, Tuple, Union
+
 from sty import RgbFg, Style, bg, ef, fg, rs
-from typing import Union, Tuple
 
 """To know more about sty module
 https://github.com/feluxe/sty
@@ -8,15 +10,26 @@ https://github.com/feluxe/sty
 fg.orange = Style(RgbFg(255, 150, 50))
 
 
-def aprint(*args, sup_err: bool = True, sep: str = " ", end: str = "\n", **kwargs):
+def aprint(
+    *args: object,
+    sup_err: Optional[bool] = True,
+    sep: Optional[str] = " ",
+    end: Optional[str] = "\n",
+    same_line: Optional[bool] = False,
+    flush: Optional[bool] = False,
+    **kwargs
+):
     """Alternative Print\n
     Eg:\n
         ("a--", (10, 15, 20), "b--", "red", "g--", "green", sep="\\n")\n
         ("Prints in green","green")
 
     Args:
-        sup_err (bool, optional): Suppress errors/exceptions showing on terminal. Defaults to False.
-        sep (str, optional): seperator used if the elements are more than 1 to print. Defaults to " ".
+        sup_err (Optional[bool], optional): Suppress errors/exceptions showing on terminal. Defaults to True.
+        sep (Optional[str], optional): String inserted between values. Defaults to " ".
+        end (Optional[str], optional): String appended after the last value. Defaults to "\\n".
+        same_line (Optional[bool], optional): . Defaults to False.
+        flush (Optional[bool], optional): Whether to forcibly flush the stream. Defaults to False.
     """
     for i in range(0, len(args), 2):
         try:
@@ -46,8 +59,12 @@ def aprint(*args, sup_err: bool = True, sep: str = " ", end: str = "\n", **kwarg
             ender = ""
         if i >= len(args) - 2:
             sep = ""
-        print(para + str(args[i]) + ender, end=sep)
-    print(end=end)
+        print(para + str(args[i]) + ender, end=sep, flush=flush)
+    if same_line:
+        print(end="\r", flush=flush)
+        sys.stdout.write("\033[K")
+    else:
+        print(end=end, flush=flush)
 
 
 def printInfo(
@@ -55,10 +72,35 @@ def printInfo(
     i_clr: Union[int, str, Tuple[int, int, int]],
     b_clr: Union[int, str, Tuple[int, int, int]],
     *args,
-    sup_err: bool = True,
-    sep: str = " ",
-    end: str = "\n",
+    sup_err: Optional[bool] = True,
+    sep: Optional[str] = " ",
+    end: Optional[str] = "\n",
+    same_line: Optional[bool] = False,
+    flush: Optional[bool] = False,
     **kwargs
 ):
-    aprint("[", b_clr, i_sym, i_clr, "] ", b_clr, sep="", end="")
-    aprint(*args, sup_err=sup_err, sep=sep, end=end)
+    """
+
+    Args:
+        i_sym (str): Info symbol.
+        i_clr (Union[int, str, Tuple[int, int, int]]): Info colour.
+        b_clr (Union[int, str, Tuple[int, int, int]]): Brackets colour.
+        sup_err (Optional[bool], optional): Suppress errors/exceptions showing on terminal. Defaults to True.
+        sep (Optional[str], optional): String inserted between values. Defaults to " ".
+        end (Optional[str], optional): String appended after the last value. Defaults to "\\n".
+        same_line (Optional[bool], optional): . Defaults to False.
+        flush (Optional[bool], optional): Whether to forcibly flush the stream. Defaults to False.
+    """
+    aprint("[", b_clr, i_sym, i_clr, "] ", b_clr, sep="", end="", flush=flush)
+    aprint(*args, sup_err=sup_err, sep=sep, end=end, same_line=same_line, flush=flush)
+
+
+def deleteLines(n=1):
+    """Delete 'n' last lines
+
+    Args:
+        n (int, optional): number of lines. Defaults to 1.
+    """
+    for _ in range(n):
+        sys.stdout.write("\x1b[1A")  # cursor up
+        sys.stdout.write("\x1b[2K")  # erase line

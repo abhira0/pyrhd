478345f95a6b75ad5a7a9139e1683e79c3c694d9
abhira0 <abhishekr0@protonmail.com>
2021-12-18 22:05:45 +0530

    Update dupy.py
    1. hm is now a instance variable
    2. supports saveflag helps in stopping the deamon thread

--- pyrhd/postproc/dupy.py
+++ pyrhd/postproc/dupy.py
@@ -1,5 +1,8 @@
+import copy
 import hashlib
 import os
+import time
+from threading import Thread
 
 from pyrhd.utility.utils import Utils
 
@@ -8,7 +11,16 @@ class Dupy:
     def __init__(self, root_path) -> None:
         self.root_path = root_path
         self.dups = []
+        self.hm = {}
+        self.saverthread = Thread(target=self.saveDeamon)
+        self.saveflag = True
+        self.saverthread.start()
+
         self.main()
+        self.saveflag = False
+        self.saverthread.join()
+        cache_path = os.path.join(self.root_path, "dupy.json")
+        Utils.json.saveDict(copy.deepcopy(self.hm), cache_path)
 
     def getHash(self, file_path, first_chunk_only=True) -> str:
         with open(file_path, "rb") as f:
@@ -17,37 +29,46 @@ class Dupy:
         return hashed
 
     def main(self):
-        hm = dict()
+        self.hm = dict()
         # size based hashmap
         for i in Utils.os.getAllFiles(self.root_path):
             file_size = os.path.getsize(i)
-            hm[file_size] = hm.get(file_size, {0: [], 1: {}, 2: {}})
-            hm[file_size][0].append(i)
+            self.hm[file_size] = self.hm.get(file_size, {0: [], 1: {}, 2: {}})
+            self.hm[file_size][0].append(i)
+        print(len(self.hm))
 
         # first 1024 bytes based hashmap
-        for i, j in ((i, j) for (i, j) in hm.items() if len(j[0]) > 1):
+        for i, j in ((i, j) for (i, j) in self.hm.items() if len(j[0]) > 1):
             for file_path in j[0]:
                 hashed = self.getHash(file_path)
-                hm[i][1][hashed] = hm[i][1].get(hashed, [])
-                hm[i][1][hashed].append(file_path)
+                self.hm[i][1][hashed] = self.hm[i][1].get(hashed, [])
+                self.hm[i][1][hashed].append(file_path)
 
         # full-file based hashmap
-        for i, j in hm.items():
+        for i, j in self.hm.items():
             for a, b in j[1].items():
-                for file_path in b:
-                    hashed = self.getHash(file_path, False)
-                    hm[i][2][hashed] = hm[i][2].get(hashed, [])
-                    hm[i][2][hashed].append(file_path)
+                if len(b) > 1:
+                    for file_path in b:
+                        hashed = self.getHash(file_path, False)
+                        self.hm[i][2][hashed] = self.hm[i][2].get(hashed, [])
+                        self.hm[i][2][hashed].append(file_path)
 
         # analysis
         count = 0
-        for size, size_based in hm.items():
+        for size, size_based in self.hm.items():
             for file, file_based in size_based[2].items():
                 count += 1
                 if len(file_based) > 1:
                     self.dups.append(file_based)
+
+    def saveDeamon(self):
         cache_path = os.path.join(self.root_path, "dupy.json")
-        Utils.json.saveDict(hm, cache_path)
+        while self.saveflag:
+            try:
+                Utils.json.saveDict(copy.deepcopy(self.hm), cache_path)
+            except:
+                ...
+            time.sleep(5)
 
     def getDups(self):
         return self.dups
